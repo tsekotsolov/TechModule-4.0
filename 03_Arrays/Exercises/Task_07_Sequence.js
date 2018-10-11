ea0652a294a81a10684938743f5ef7e257fe29e4
Tseko Tsolov <tsekotsolov@gmail.com>
2018-10-11 23:05:20 +0300

    task 7 second solution added

--- 03_Arrays/Exercises/Task_07_Sequence.js
+++ 03_Arrays/Exercises/Task_07_Sequence.js
@@ -1,5 +1,5 @@
-function solve (input) {
-  
+function solve1 (input) {
+
   let arr = input[0].split(' ').map(Number)
   let counter = 1
   let arrResult = []
@@ -31,8 +31,30 @@ function solve (input) {
   console.log(result.join(' '))
 }
 
-solve(['2 1 1 1 1 1 2 2 3 3 2 2 2 1 4 4 4 4 4 4'])
-// solve(['1 1 1 2 3 1 3 3'])
-// solve(['4 4 4 4'])
-// solve(['0 1 1 5 2 2 6 3 3'])
-// solve(['2'])
+function solve2 (input) {
+  let arr = input[0].split(' ').map(Number)
+
+  let counter = 1
+  let countArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      counter++
+    } else {
+      counter = 1
+    }
+    countArr.push(counter)
+  }
+
+  let index = countArr.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)
+
+  let valueToRepeat = arr[index]
+  let timeToRepeat = countArr[index]
+
+  let result = []
+
+  for (let i = 0; i < timeToRepeat; i++) {
+    result.push(valueToRepeat)
+  }
+
+  console.log(result.join(' '))
+}
