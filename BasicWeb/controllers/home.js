18351a5e753c2e99b418531fb44db4eceae205e0
Tseko Tsolov <tsekotsolov@gmail.com>
2018-11-26 10:38:05 +0200

    up

--- BasicWeb/controllers/home.js
+++ BasicWeb/controllers/home.js
@@ -1,11 +1,19 @@
+const Calculator = require('../model/calculator')
+
 module.exports = {
   indexGet: (req, res) => {
     res.render('home/index')
   },
 
   indexPost: (req, res) => {
-    console.log(req.body)
-    res.render('home/index')
+    const leftOperand = Number(req.body.calculator.leftOperand)
+    const rightOperand = Number(req.body.calculator.rightOperand)
+    const operator = req.body.calculator.operator
+    let calculator = new Calculator(leftOperand, operator, rightOperand)
+    let result = calculator.calculate()
+
+    res.render('home/index', {calculator: {leftOperand, rightOperand, operator},
+      result})
   }
 
 }
--- BasicWeb/model/calculator.js
+++ BasicWeb/model/calculator.js
@@ -1,5 +1,25 @@
-class Calculator (){
-    constructor(){
-        
+class Calculator {
+  constructor (leftOperand, operator, rightOperand) {
+    this.leftOperand = leftOperand
+    this.operator = operator
+    this.rightOperand = rightOperand
+  }
+
+  calculate () {
+    switch (this.operator) {
+      case '*':
+        return this.leftOperand * this.rightOperand
+
+      case '/':
+        return this.leftOperand / this.rightOperand
+
+      case '+':
+        return this.leftOperand + this.rightOperand
+
+      default:
+        return this.leftOperand - this.rightOperand
     }
+  }
 }
+
+module.exports = Calculator
