6eefbd7db6000ff5515c57dee14d14ca37601c58
Tseko Tsolov <tsekotsolov@gmail.com>
2018-12-02 10:16:12 +0200

    task solved

--- Meister_Task/controllers/task-controller.js
+++ Meister_Task/controllers/task-controller.js
@@ -1,23 +1,68 @@
+const Task = require('../models/Task')
 module.exports = {
   getIndex: function (req, res) {
-    return res.render('index')
+    Task.find({}).then((allTasks) => {
+      const openTasks = allTasks.filter(t => t.status === 'Open')
+      const inProgressTasks = allTasks.filter(t => t.status === 'In Progress')
+      const finishedTasks = allTasks.filter(t => t.status === 'Finished')
+      return res.render('index', {openTasks, inProgressTasks, finishedTasks})
+    })
   },
   getCreate: function (req, res) {
-    // TODO: render the create view
+    return res.render('create')
   },
   postCreate: function (req, res) {
-    // TODO: create new task and redirect to the index view
+    if (req.body.title && req.body.status) {
+      Task.create(req.body)
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    }
+    return res.redirect('/')
   },
   getEdit: function (req, res) {
-    // TODO: get the task for editing, pass it to the edit view and render it
+    const id = req.params.id
+    Task.findById(id).then((task) => {
+      switch (task.status) {
+        case 'Open':
+          task.open = true
+          break
+        case 'In Progress':
+          task.inProgress = true
+          break
+        default:
+          task.finished = true
+          break
+      }
+      return res.render('edit', {task})
+    })
   },
   postEdit: function (req, res) {
-    // TODO: update the task and redirect to index view
+    const id = req.params.id
+    Task.findByIdAndUpdate(id, req.body)
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   },
   getDelete: function (req, res) {
-    // TODO: get the task for deleting, pass it to the detele view and render it
+    const id = req.params.id
+    Task.findById(id).then((task) => {
+      switch (task.status) {
+        case 'Open':
+          task.open = true
+          break
+        case 'In Progress':
+          task.inProgress = true
+          break
+        default:
+          task.finished = true
+          break
+      }
+      return res.render('delete', {task})
+    })
   },
   postDelete: function (req, res) {
-    // TODO: delete the task and redirect to the index view
+    const id = req.params.id
+    Task.findByIdAndDelete(id)
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
   }
 }
---
+++ Meister_Task/models/Task.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    reqired: true,
+    enum: ['In Progress', 'Finished', 'Open']
+  }
+})
+
+const Task = mongoose.model('Tasks', taskSchema)
+module.exports = Task
