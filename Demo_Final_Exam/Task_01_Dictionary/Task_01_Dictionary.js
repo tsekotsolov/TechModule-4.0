ef26bd802b0d89cebdf66c9ebf7c0a5e40cd090a
Tseko Tsolov <tsekotsolov@gmail.com>
2018-12-08 09:22:16 +0200

    up

--- Demo_Final_Exam/Task_01_Dictionary/Task_01_Dictionary.js
+++ Demo_Final_Exam/Task_01_Dictionary/Task_01_Dictionary.js
@@ -1,22 +1,16 @@
 function solve (input) {
-  const dict = input[0].split(' | ')
+  const dictionaryRawData = input[0].split(' | ')
   const controlWords = input[1].split(' | ')
   const command = input[2]
-
   const dictionary = {}
 
-  for (const line of dict) {
-    const [word, definition] = line.split(': ')
-    if (dictionary.hasOwnProperty(word)) {
-      const currentDefinitions = dictionary[word]
-      currentDefinitions.push(definition)
-    } else {
-      dictionary[word] = [definition]
+  for (const entry of dictionaryRawData) {
+    const [word, definition] = entry.split(': ')
+    if (!dictionary.hasOwnProperty(word)) {
+      dictionary[word] = []
     }
-  }
-  for (const word in dictionary) {
-    const sorted = dictionary[word].sort((a, b) => b.length - a.length)
-    dictionary[word] = sorted
+    dictionary[word].push(definition)
+    dictionary[word].sort((a, b) => b.length - a.length)
   }
   for (const word of controlWords) {
     if (dictionary.hasOwnProperty(word)) {
@@ -32,12 +26,12 @@ function solve (input) {
   }
 }
 
-solve(
-  ['programmer: an animal, which turns coffee into code | developer: a magician',
-    'Pesho | Gosho',
-    'List']
-)
+// solve(
+//   ['programmer: an animal, which turns coffee into code | developer: a magician',
+//     'Pesho | Gosho',
+//     'List']
+// )
 
-// solve(['tackle: the equipment required for a task or sport | code: write code for a computer program | bit: a small piece, part, or quantity of something | tackle: make determined efforts to deal with a problem | bit: a short time or distance',
-//   'bit | code | tackle',
-//   'End'])
+solve(['tackle: the equipment required for a task or sport | code: write code for a computer program | bit: a small piece, part, or quantity of something | tackle: make determined efforts to deal with a problem | bit: a short time or distance',
+  'bit | code | tackle',
+  'End'])
